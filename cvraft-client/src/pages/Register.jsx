941e7a371e308ef7bc7f3418b2acf25003bea70b
Rohith S <rohithsivakumar1205@gmail.com>
2026-05-25 18:47:36 +0530

    chore: change name placeholder to John Doe on registration page

--- cvraft-client/src/pages/Register.jsx
+++ cvraft-client/src/pages/Register.jsx
@@ -90,7 +90,7 @@ const Register = () => {
                 name="name"
                 value={form.name}
                 onChange={handleChange}
-                placeholder="Rohith Sivakumar"
+                placeholder="John Doe"
                 className="w-full border border-gray-200 rounded-xl px-4
                 py-3 text-sm focus:outline-none focus:ring-2
                 focus:ring-blue-400 focus:border-transparent"
